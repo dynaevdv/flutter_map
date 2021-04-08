443bd8d83ef53e06fbe5f01d4b2bb8deb5d11d2b
Denis Dunaev <ddunaev@ac-sw.com>
2021-04-08 15:49:02 +0700

    Added padding to x,y,z

--- lib/src/layer/tile_provider/tile_provider.dart
+++ lib/src/layer/tile_provider/tile_provider.dart
@@ -22,9 +22,9 @@ abstract class TileProvider {
     var z = _getZoomForUrl(coords, options);
 
     var data = <String, String>{
-      'x': coords.x.round().toString(),
-      'y': coords.y.round().toString(),
-      'z': z.round().toString(),
+      'x': coords.x.round().toString().padLeft(8, '0'),
+      'y': coords.y.round().toString().padLeft(8, '0'),
+      'z': z.round().toString().padLeft(2, '0'),
       's': getSubdomain(coords, options),
       'r': '@2x',
     };
